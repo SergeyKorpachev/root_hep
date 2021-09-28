7240b1855a60e492ce4cdc987d2bcb834909f03b
SergeyKorpachev <sergey.korpachev@desy.de>
2021-09-28 20:38:16 +0300

    Add files via upload

--- code99999.c
+++ code99999.c
@@ -73,24 +73,20 @@ void code() {
     
     for(int i = 0; i < n; i++) {
         weights[i] = 0.0;
-    }
-    
-    Float_t sum = 0.0;
-    
-    FILE *file1 = fopen("test.txt", "r");
-    if (!(long)file1) return;
-    
-    for (int i = 0; i < n; i++) { // or %f
-      fscanf( file1, "%E\n", &weights[i]);
-      if (feof(file1)) break;
-    }
-    
-    for(int i = 0; i < n; i++) {
-        printf("Weights === %E\n", weights[i]);
-        sum = sum + weights[i];
-    }
-    
-    printf("Sum === %E\n", sum);
+    }{
+        
+        static TStyle* atlasStyle = 0;
+
+        std::cout << "\nApplying ATLAS style settings...\n" << std::endl ;
+
+        if ( atlasStyle==0 ) atlasStyle = AtlasStyle();
+
+        gROOT->SetStyle("ATLAS");
+
+        gROOT->ForceStyle();
+
+      }
+
     
         
     
