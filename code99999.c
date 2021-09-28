f761e062aa31af9815c2d292b393e75e40045119
SergeyKorpachev <sergey.korpachev@desy.de>
2021-09-28 20:34:41 +0300

    Add files via upload

--- code99999.c
+++ code99999.c
@@ -1,5 +1,72 @@
 void code() {
     
+    Float_t sum = 0.0;
+    
+    FILE *file1 = fopen("test.txt", "r");
+    if (!(lfile1, "%E\n", &weights[i]);
+      if (feof(file1)) break;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        printf("Weights === %E\n", weights[i]);
+        sum = sum + weights[i];
+    }
+    
+    printf("Sum === %E\n", sum);
+    
+    
+    const Int_t n = 29;
+
+    Float_t weights[n];
+    
+    for(int i = 0; i < n; i++) {
+        weights[i] = 0.0;
+    }
+    
+    Float_t sum = 0.0;
+    
+    FILE *file1 = fopen("test.txt", "r");
+    if (!(long)file1) return;
+    
+    for (int i = 0; i < n; i++) { // or %f
+      fscanf( file1, "%E\n", &weights[i]);
+      if (feof(file1)) break;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        printf("Weights === %E\n", weights[i]);
+        sum = sum + weights[i];
+    }
+    
+    printf("Sum === %E\n", sum);
+    
+    
+    const Int_t n = 29;
+
+    Float_t weights[n];
+    
+    for(int i = 0; i < n; i++) {
+        weights[i] = 0.0;
+    }
+    
+    Float_t sum = 0.0;
+    
+    FILE *file1 = fopen("test.txt", "r");
+    if (!(long)file1) return;
+    
+    for (int i = 0; i < n; i++) { // or %f
+      fscanf( file1, "%E\n", &weights[i]);
+      if (feof(file1)) break;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        printf("Weights === %E\n", weights[i]);
+        sum = sum + weights[i];
+    }
+    
+    printf("Sum === %E\n", sum);
+    
+    
     const Int_t n = 29;
 
     Float_t weights[n];
